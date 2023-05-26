267efd395b5a905d86c8319271f153e36432d0f9
Orsi Suto <orsolya.suto.so@gmail.com>
2023-05-27 00:11:57 +0200

    Updates

---
+++ precache-manifest.f3f4ac32d40ce2265d5d68b71a07815f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0c58c766c7ae96a5081",
+    "url": "/robofriends/static/css/main.c3a04b9e.chunk.css"
+  },
+  {
+    "revision": "f0c58c766c7ae96a5081",
+    "url": "/robofriends/static/js/main.f0c58c76.chunk.js"
+  },
+  {
+    "revision": "4d80ca7727d6ffc9828c",
+    "url": "/robofriends/static/css/1.e9c089eb.chunk.css"
+  },
+  {
+    "revision": "4d80ca7727d6ffc9828c",
+    "url": "/robofriends/static/js/1.4d80ca77.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "aae0e380b2637d3cb3cd5188306087d6",
+    "url": "/robofriends/index.html"
+  }
+];
